e5ac746160e2feae1e5a4f102c34a4f8c6951421
lucastaglia <lucastaglia@hotmail.com>
2014-12-01 09:58:37 -0200

    Versão Final

--- docs/tweeter.sql
+++ docs/tweeter.sql
@@ -1,176 +1,130 @@
---
--- Base de Dados: `tweeter`
---
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `tweeter`.`usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tweeter`.`usuarios` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(60) NULL,
+  `login` VARCHAR(20) NULL,
+  `senha` VARCHAR(60) NULL,
+  `email` VARCHAR(60) NULL,
+  `data_cadastro` DATE NULL,
+  `foto` VARCHAR(50) NULL,
+  `descricao` TEXT NULL,
+  PRIMARY KEY (`codigo`))
+ENGINE = InnoDB;
 
---
--- Estrutura da tabela `favoritos`
---
 
-CREATE TABLE IF NOT EXISTS `favoritos` (
-  `codigo_tweet` int(11) NOT NULL,
-  `codigo_usuario` int(11) NOT NULL,
-  PRIMARY KEY (`codigo_tweet`,`codigo_usuario`),
-  KEY `fk_favoritos_tweets1_idx` (`codigo_tweet`),
-  KEY `fk_favoritos_usuarios1` (`codigo_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `mensagens`
---
-
-CREATE TABLE IF NOT EXISTS `mensagens` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `texto` text,
-  `imagem` varchar(50) DEFAULT NULL,
-  `data_hora_envio` timestamp NULL DEFAULT NULL,
-  `remetente` int(11) NOT NULL,
-  `destinatario` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `tweeter`.`tweets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tweeter`.`tweets` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `texto` VARCHAR(140) NULL,
+  `data_hora_postagem` TIMESTAMP NULL,
+  `imagem` VARCHAR(50) NULL,
+  `codigo_usuario` INT NOT NULL,
   PRIMARY KEY (`codigo`),
-  KEY `fk_mensagens_usuarios1_idx` (`remetente`),
-  KEY `fk_mensagens_usuarios2_idx` (`destinatario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  INDEX `fk_tweets_usuarios_idx` (`codigo_usuario` ASC),
+  CONSTRAINT `fk_tweets_usuarios`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `tweeter`.`usuarios` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `respostas`
---
-
-CREATE TABLE IF NOT EXISTS `respostas` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `texto` varchar(140) DEFAULT NULL,
-  `imagem` varchar(50) DEFAULT NULL,
-  `data_hora_postagem` timestamp NULL DEFAULT NULL,
-  `codigo_tweet` int(11) NOT NULL,
-  `codigo_usuario` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `tweeter`.`respostas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tweeter`.`respostas` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `texto` VARCHAR(140) NULL,
+  `imagem` VARCHAR(50) NULL,
+  `data_hora_postagem` TIMESTAMP NULL,
+  `codigo_tweet` INT NOT NULL,
+  `codigo_usuario` INT NOT NULL,
   PRIMARY KEY (`codigo`),
-  KEY `fk_respostas_tweets1_idx` (`codigo_tweet`),
-  KEY `fk_respostas_usuarios1_idx` (`codigo_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  INDEX `fk_respostas_tweets1_idx` (`codigo_tweet` ASC),
+  INDEX `fk_respostas_usuarios1_idx` (`codigo_usuario` ASC),
+  CONSTRAINT `fk_respostas_tweets1`
+    FOREIGN KEY (`codigo_tweet`)
+    REFERENCES `tweeter`.`tweets` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_respostas_usuarios1`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `tweeter`.`usuarios` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `retweets`
---
+-- -----------------------------------------------------
+-- Table `tweeter`.`retweets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tweeter`.`retweets` (
+  `data_hora_postagem` TIMESTAMP NOT NULL,
+  `codigo_tweet` INT NOT NULL,
+  `codigo_usuario` INT NOT NULL,
+  PRIMARY KEY (`codigo_tweet`, `codigo_usuario`),
+  INDEX `fk_retweets_usuarios1_idx` (`codigo_usuario` ASC),
+  CONSTRAINT `fk_retweets_tweets1`
+    FOREIGN KEY (`codigo_tweet`)
+    REFERENCES `tweeter`.`tweets` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_retweets_usuarios1`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `tweeter`.`usuarios` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `retweets` (
-  `data_hora_postagem` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `codigo_tweet` int(11) NOT NULL,
-  `codigo_usuario` int(11) NOT NULL,
-  PRIMARY KEY (`codigo_tweet`,`codigo_usuario`),
-  KEY `fk_retweets_usuarios1_idx` (`codigo_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `tweeter`.`favoritos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tweeter`.`favoritos` (
+  `codigo_tweet` INT NOT NULL,
+  `codigo_usuario` INT NOT NULL,
+  PRIMARY KEY (`codigo_tweet`, `codigo_usuario`),
+  INDEX `fk_favoritos_tweets1_idx` (`codigo_tweet` ASC),
+  CONSTRAINT `fk_favoritos_usuarios1`
+    FOREIGN KEY (`codigo_usuario`)
+    REFERENCES `tweeter`.`usuarios` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_favoritos_tweets1`
+    FOREIGN KEY (`codigo_tweet`)
+    REFERENCES `tweeter`.`tweets` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Estrutura da tabela `seguidores`
---
 
-CREATE TABLE IF NOT EXISTS `seguidores` (
-  `codigo_seguidor` int(11) NOT NULL,
-  `codigo_seguido` int(11) NOT NULL,
-  PRIMARY KEY (`codigo_seguidor`,`codigo_seguido`),
-  KEY `codigo_seguido` (`codigo_seguido`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `seguidores`
---
-
-INSERT INTO `seguidores` (`codigo_seguidor`, `codigo_seguido`) VALUES
-(3, 1),
-(1, 2);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tweets`
---
-
-CREATE TABLE IF NOT EXISTS `tweets` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `texto` varchar(140) DEFAULT NULL,
-  `data_hora_postagem` timestamp NULL DEFAULT NULL,
-  `imagem` varchar(50) DEFAULT NULL,
-  `codigo_usuario` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `tweeter`.`mensagens`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tweeter`.`mensagens` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `texto` TEXT NULL,
+  `imagem` VARCHAR(50) NULL,
+  `data_hora_envio` TIMESTAMP NULL,
+  `remetente` INT NOT NULL,
+  `destinatario` INT NOT NULL,
   PRIMARY KEY (`codigo`),
-  KEY `fk_tweets_usuarios_idx` (`codigo_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  INDEX `fk_mensagens_usuarios1_idx` (`remetente` ASC),
+  INDEX `fk_mensagens_usuarios2_idx` (`destinatario` ASC),
+  CONSTRAINT `fk_mensagens_usuarios1`
+    FOREIGN KEY (`remetente`)
+    REFERENCES `tweeter`.`usuarios` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_mensagens_usuarios2`
+    FOREIGN KEY (`destinatario`)
+    REFERENCES `tweeter`.`usuarios` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(60) DEFAULT NULL,
-  `login` varchar(20) DEFAULT NULL,
-  `senha` varchar(60) DEFAULT NULL,
-  `email` varchar(60) DEFAULT NULL,
-  `data_cadastro` date DEFAULT NULL,
-  `foto` varchar(50) DEFAULT NULL,
-  `descricao` text,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`codigo`, `nome`, `login`, `senha`, `email`, `data_cadastro`, `foto`, `descricao`) VALUES
-(1, 'José da Silva', 'zedasilva', '123456', 'zedasilva@gmail.com', NULL, NULL, NULL),
-(2, 'Fulano de Tal', 'fulanodetal', '123456', 'fulano@gmail.com', NULL, NULL, 'blá, blá, blá...'),
-(3, 'João dos Santos', 'jsantos', '12345678', 'dossantos@gmail.com', NULL, NULL, NULL);
-
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `favoritos`
---
-ALTER TABLE `favoritos`
-  ADD CONSTRAINT `fk_favoritos_tweets1` FOREIGN KEY (`codigo_tweet`) REFERENCES `tweets` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_favoritos_usuarios1` FOREIGN KEY (`codigo_usuario`) REFERENCES `usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Limitadores para a tabela `mensagens`
---
-ALTER TABLE `mensagens`
-  ADD CONSTRAINT `fk_mensagens_usuarios1` FOREIGN KEY (`remetente`) REFERENCES `usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_mensagens_usuarios2` FOREIGN KEY (`destinatario`) REFERENCES `usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Limitadores para a tabela `respostas`
---
-ALTER TABLE `respostas`
-  ADD CONSTRAINT `fk_respostas_tweets1` FOREIGN KEY (`codigo_tweet`) REFERENCES `tweets` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_respostas_usuarios1` FOREIGN KEY (`codigo_usuario`) REFERENCES `usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Limitadores para a tabela `retweets`
---
-ALTER TABLE `retweets`
-  ADD CONSTRAINT `fk_retweets_tweets1` FOREIGN KEY (`codigo_tweet`) REFERENCES `tweets` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_retweets_usuarios1` FOREIGN KEY (`codigo_usuario`) REFERENCES `usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Limitadores para a tabela `seguidores`
---
-ALTER TABLE `seguidores`
-  ADD CONSTRAINT `seguidores_ibfk_2` FOREIGN KEY (`codigo_seguido`) REFERENCES `usuarios` (`codigo`),
-  ADD CONSTRAINT `seguidores_ibfk_1` FOREIGN KEY (`codigo_seguidor`) REFERENCES `usuarios` (`codigo`);
-
---
--- Limitadores para a tabela `tweets`
---
-ALTER TABLE `tweets`
-  ADD CONSTRAINT `fk_tweets_usuarios` FOREIGN KEY (`codigo_usuario`) REFERENCES `usuarios` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
